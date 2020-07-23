4881fedfb5e0f10a2e438e1219e1c6aa2a17f91d
Uilton Gomes <uilton-gomes@hotmail.com>
2020-07-23 12:08:20 -0300

    Criação de Metódos Update e Delete

--- App/Dao/ClienteDao.php
+++ App/Dao/ClienteDao.php
@@ -4,42 +4,76 @@ namespace App\Dao;
 
 use App\Model\Cliente;
 use App\Model\Connection;
+use Exception;
 
 class ClienteDao
 {
     public function register(Cliente $cliente)
     {
+        try {
+            $sql = "INSERT INTO CLIENTE(nome, email, telefone, cpf) VALUES(?,?,?,?)";
+            $cmd = Connection::getConexao()->prepare($sql);
+            $cmd->bindValue(1, $cliente->getNome());
+            $cmd->bindValue(2, $cliente->getEmail());
+            $cmd->bindValue(3, $cliente->getTelefone());
+            $cmd->bindValue(4, $cliente->getCpf());
+            $cmd->execute();
 
-        $sql = "INSERT INTO CLIENTE(nome, email, telefone, cpf) VALUES(?,?,?,?)";
-        $cmd = Connection::getConexao()->prepare($sql);
-        $cmd->bindValue(1, $cliente->getNome());
-        $cmd->bindValue(2, $cliente->getEmail());
-        $cmd->bindValue(3, $cliente->getTelefone());
-        $cmd->bindValue(4, $cliente->getCpf());
-        $cmd->execute();
+            $result = ["message" => "Sucesso!"];
 
+        } catch (Exception $e) {
+            $result = ["message" => "Erro ao conectar-se ao servidor de banco de dados.", "error" => "$e"];
+        }
+        return $result;
     }
     function list() {
+        try {
+            $sql = "SELECT *FROM CLIENTE";
+            $cmd = Connection::getConexao()->prepare($sql);
+            $cmd->execute();
 
-        $sql = "SELECT *FROM CLIENTE";
-        $cmd = Connection::getConexao()->prepare($sql);
-        $cmd->execute();
-
-        if ($cmd->rowCount() > 0):
-            $result = $cmd->fetchAll(\PDO::FETCH_OBJ);
-        else:
-            $result = ["message"=>"Nenhum Registro Encontrado."];
-        endif;
-
+            if ($cmd->rowCount() > 0):
+                $result = $cmd->fetchAll(\PDO::FETCH_OBJ);
+            else:
+                $result = ["message" => "Nenhum Registro Encontrado."];
+            endif;
+        } catch (Exception $e) {
+            $result = ["message" => "Erro ao conectar-se ao servidor de banco de dados.", "error" => "$e"];
+        }
         return $result;
     }
     public function update(Cliente $cliente)
     {
+        try {
+            $sql = "UPDATE CLIENTE SET nome = ? , email = ?, telefone = ?, cpf = ?
+                WHERE cod = ?";
+            $cmd = Connection::getConexao()->prepare($sql);
+            $cmd->bindValue(1, $cliente->getNome());
+            $cmd->bindValue(2, $cliente->getEmail());
+            $cmd->bindValue(3, $cliente->getTelefone());
+            $cmd->bindValue(4, $cliente->getCpf());
+            $cmd->bindValue(5, $cliente->getCod());
+            $cmd->execute();
 
+            $result = ["message" => "Sucesso!"];
+
+        } catch (Exception $e) {
+            $result = ["message" => "Erro ao conectar-se ao servidor banco de dados.", "error" => "$e"];
+        }
+        return $result;
     }
-    public function delete()
+    public function delete($cod)
     {
+        try {
+            $sql = "DELETE FROM CLIENTE WHERE cod = ?";
+            $cmd = Connection::getConexao()->prepare($sql);
+            $cmd->bindValue(1, $cod);
+            $cmd->execute();
 
+            $result = ["message" => "Cliente excluído(a) com sucesso!"];
+        } catch (Exception $e) {
+            $result = ["message" => "Erro ao conectar-se ao servidor de banco de dados.", "error" => "$e"];
+        }
     }
 
 }
--- index.php
+++ index.php
@@ -2,16 +2,22 @@
 require_once 'vendor/autoload.php';
 
 $cliente = new \App\Model\Cliente;
+$cliente2 = new \App\Model\Cliente;
 
-// $cliente->setNome('Marta');
-// $cliente->setCpf('025.244.452-23');
-// $cliente->setEmail('teste@teste.com.br');
-// $cliente->setTelefone('(71)99351-5493');
+$cliente->setNome('João carlos');
+$cliente->setCpf('033.523.333-08');
+$cliente->setEmail('jc@gmail.com');
+$cliente->setTelefone('(71)98451-5412');
 
-// $clienteDap = new \App\Dao\ClienteDao;
-// $clienteDap->register($cliente);
+$cliente2->setNome('Marcos Dantas');
+$cliente2->setCpf('021.254.854-32');
+$cliente2->setEmail('mdantas@hotmail.com');
+$cliente2->setTelefone('(73)98121-2254');
+
+$clienteDap = new \App\Dao\ClienteDao;
+$clienteDap->register($cliente);
+$clienteDap->register($cliente2);
 
 
 $clienteDao = new \App\Dao\ClienteDao;
-
-echo json_encode($clienteDao->list());
+echo json_encode($clienteDap->list());
